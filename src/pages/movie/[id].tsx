be8e176f7fab67ad24484b9fb82cf069e8b463ab
Yurii Hodynskyi <y.hodynskyi@eisgroup.com>
2022-08-14 16:49:48 +0300

    Home task 10: Implemented functionality for moviePage with JavaScript disabled

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -5,21 +5,70 @@ import { Header } from '../../shared/header'
 import { MovieDetails } from '../../shared/movie-details'
 import { Footer } from '../../shared/footer'
 import { ErrorBoundary } from '../../shared/error-boundaries'
-import { getMovieById } from '../../utils'
+import { concatMovieGenres, configureRuntime, getFullYear, getMovieById, getMovieImage } from '../../utils'
 import { IMovieItem } from '../../store/actions-types'
+import classnames from 'classnames'
+import styles from '../../shared/movie-details/MovieDetails.module.scss'
+import { Logo } from '../../components/logo'
+import { Button, ButtonTypes } from '../../components/button'
+import { SEARCH_ICON_29_30 } from '../../assets/svg'
+import React from 'react'
 
 interface IMovie {
-    movie: IMovieItem
+    movieInfo: IMovieItem
 }
 
-const Movie: NextPage<IMovie> = ({movie}) => {
+const Movie: NextPage<IMovie> = ({movieInfo}) => {
 
     return (
         <ErrorBoundary errorText={'Oops! Something went wrong. Please try again later.'}>
             <MovieModalContextProvider>
                 <MovieDetailsContextProvider>
-                    <Header/>
-                    <MovieDetails movieDetails={movie}/>
+                    <div className={classnames('container', styles.detailsContent)}>
+                        <div className={classnames('row', 'align-items-start', 'justify-content-between')}>
+                            <div className="col">
+                                <Logo/>
+                            </div>
+                            <div className="col" />
+                        </div>
+                        <div className={classnames('row', 'align-items-start', styles.movieDetails)}>
+                            <div className="col col-4">
+                                <div className={styles.movieImage}>
+                                    <img
+                                        src={movieInfo.poster_path}
+                                        alt={movieInfo.title}
+                                        onError={({ currentTarget }) => {
+                                            const defaultImage = getMovieImage('placeholder_320x480.png')
+                                            currentTarget.onerror = null // prevents looping
+                                            currentTarget.src=defaultImage
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="col col-8">
+                                <div className="row nowrap align-items-center">
+                                    <p className={classnames(styles.movieTitle, 'h1', 'col')}>{movieInfo.title}</p>
+                                    <div className="col">
+                                        <p className={classnames(styles.movieRating, 'col')}>
+                                            <span>{movieInfo.vote_average}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <p className={styles.movieGenre}>{concatMovieGenres(movieInfo.genres)}</p>
+                                <div className="row">
+                                    <div className="col">
+                                        <p className={styles.movieRelease}>{getFullYear(movieInfo.release_date)}</p>
+                                    </div>
+                                    <div className="col">
+                                        <p className={styles.movieDuration}>{configureRuntime(movieInfo.runtime)}</p>
+                                    </div>
+                                    <div className={classnames('col', 'col-12')}>
+                                        <p className={styles.movieDescription}>{movieInfo.overview}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Footer/>
                 </MovieDetailsContextProvider>
             </MovieModalContextProvider>
@@ -29,10 +78,10 @@ const Movie: NextPage<IMovie> = ({movie}) => {
 
 export const getServerSideProps = async (context: any) => {
     const {id} = context.query
-    const movie = await getMovieById(id)
+    const movieInfo = await getMovieById(id)
 
     return {
-        props: {movie}
+        props: {movieInfo}
     }
 }
 
